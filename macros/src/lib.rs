d63e2b7e8339a569b47d6d8a52a051d3b5e89e57
John Bretz <johnfish218@gmail.com>
2020-01-08 13:41:53 -0600

    Builder supports repeated fields

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,171 +1,240 @@
 extern crate proc_macro;
 extern crate proc_macro2;
 
-use proc_macro::TokenStream;
+use proc_macro::TokenStream as Stream;
+use proc_macro2::TokenStream;
 use quote::quote;
 use std::error::Error;
-use syn::{parse, Ident, ItemStruct, Type};
+use syn::{parse, Field, GenericArgument, Ident, ItemStruct, Lit, Meta, MetaList, MetaNameValue, NestedMeta, PathArguments, Type};
 
-#[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let item: ItemStruct = parse(input).expect("failed to parse object to item struct");
+enum MacroAttribute {
+    Each,
+}
 
-    let name = &item.ident;
-    let builder_name = Ident::new(&format!("{}Builder", name), name.span());
+enum FieldType {
+    Regular,
+    Repeated,
+    Optional,
+}
 
-    let builder_struct = make_builder_struct(&item, &builder_name);
-    let struct_trait = make_struct_trait(&item, &builder_name);
-    let setters_trait = make_setters_trait(&item, &builder_name);
+#[proc_macro_derive(Builder, attributes(builder))]
+pub fn builder_derive(input: Stream) -> Stream {
+    let source: ItemStruct = parse(input).expect("failed to parse object to item struct");
+
+    let source_name = &source.ident;
+    let builder_name = Ident::new(&format!("{}Builder", source_name), source_name.span());
+
+    let mut builder_empty_fields = Vec::new();
+    let mut builder_field_definitions = Vec::new();
+    let mut builder_setters = Vec::new();
+    let mut build_guards = Vec::new();
+    let mut build_values = Vec::new();
+
+    for field in &source.fields {
+        builder_empty_fields.push(make_empty_field(&field));
+        builder_field_definitions.push(make_builder_field(&field));
+        builder_setters.push(make_setter(&field));
+        let (guard, value) = make_build_guard(&field);
+        build_guards.push(guard);
+        build_values.push(value);
+    }
 
     let gen = quote! {
-        use std::error::Error;
+      use std::error::Error;
 
-        #struct_trait
+      // Builder implementation
+      impl #builder_name {
+        // Setters for each field in the inital struct
+        #(#builder_setters)*
 
-        #builder_struct
+        // Method to resolve builder to source struct
+        fn build(mut self) -> Result<#source_name, Box<dyn Error>> {
+          // Guard for missing fields
+          #(#build_guards)*
 
-        #setters_trait
+          // Return built struct with secured values
+          Ok(#source_name {
+            #(#build_values)*
+          })
+        }
+      }
+
+      // Builder struct definition
+      pub struct #builder_name {
+        #(#builder_field_definitions)*
+      }
+
+      // Builder method on source struct to create a <source_name>Builder entity
+      impl #source_name {
+        pub fn builder() -> #builder_name {
+          #builder_name {
+            #(#builder_empty_fields)*
+          }
+        }
+      }
     };
 
     gen.into()
 }
 
-fn make_builder_struct(source: &ItemStruct, builder_name: &Ident) -> proc_macro2::TokenStream {
-    let mut field_tokens = Vec::new();
-
-    for field in &source.fields {
-        let name = &field.ident;
-        let type_ = match get_option_type(&field.ty) {
-            Ok(t) => t,
-            Err(_) => field.ty.clone(),
-        };
-        field_tokens.push(quote! {
-            #name: Option<#type_>,
-        });
-    }
-
-    quote! {
-        pub struct #builder_name {
-            #(#field_tokens)*
-        }
+fn make_empty_field(field: &Field) -> TokenStream {
+    let name = &field.ident;
+    match get_field_type(field) {
+        FieldType::Repeated => quote! { #name: Vec::new(), },
+        _ => quote! { #name: None, },
     }
 }
 
-fn make_struct_trait(source: &ItemStruct, builder_name: &Ident) -> proc_macro2::TokenStream {
-    let mut fields = Vec::new();
-    let source_name = &source.ident;
-    for field in &source.fields {
-        let name = &field.ident;
-        fields.push(quote! {
-            #name: None,
-        });
+fn make_builder_field(field: &Field) -> TokenStream {
+    let name = &field.ident;
+    let ty = &field.ty;
+    match get_field_type(field) {
+        FieldType::Regular => quote! { #name: Option<#ty>, },
+        _ => quote! { #name: #ty, },
     }
+}
 
-    quote! {
-        impl #source_name {
-            pub fn builder() -> #builder_name {
-                #builder_name {
-                    #(#fields)*
+fn make_setter(field: &Field) -> TokenStream {
+    let name = &field.ident;
+    let ty = &field.ty;
+    match get_field_type(field) {
+        FieldType::Repeated => {
+            let nested_type = guess_nested_type(ty);
+            let (_, repeated_setter) = get_macro_attribute(field).unwrap();
+            let repeated_setter_name = Ident::new(&repeated_setter, proc_macro2::Span::call_site());
+            quote! {
+                fn #repeated_setter_name(mut self, #name: #nested_type) -> Self {
+                  self.#name.push(#name);
+                  self
                 }
             }
         }
+        FieldType::Optional => {
+            let nested_type = guess_nested_type(ty);
+            quote! {
+                fn #name(mut self, #name: #nested_type) -> Self {
+                  self.#name = Some(#name);
+                  self
+                }
+            }
+        }
+        FieldType::Regular => quote! {
+            fn #name(mut self, #name: #ty) -> Self {
+              self.#name = Some(#name);
+              self
+            }
+        },
     }
 }
 
-fn make_setters_trait(source: &ItemStruct, builder_name: &Ident) -> proc_macro2::TokenStream {
-    let mut setters = Vec::new();
+fn make_build_guard(field: &Field) -> (TokenStream, TokenStream) {
+    let name = &field.ident;
+    let guard = match get_field_type(field) {
+        FieldType::Regular => quote! {
+          let #name = match self.#name {
+            Some(value) => value,
+            None => return Err(From::from(format!("missing {} to build", stringify!(#name)))),
+          };
+        },
+        _ => quote! { let #name = self.#name; },
+    };
+    (guard, quote! { #name: #name, })
+}
 
-    for field in &source.fields {
-        let name = &field.ident;
-        let type_ = match get_option_type(&field.ty) {
-            Ok(t) => t,
-            Err(_) => field.ty.clone()
+fn get_field_type(field: &Field) -> FieldType {
+    match get_macro_attribute(field) {
+        Some((MacroAttribute::Each, _)) => FieldType::Repeated,
+        None => match guess_nested_type(&field.ty) {
+            Some(_) if get_type_name(&field.ty).expect("type should be nested") == "Option" => {
+                FieldType::Optional
+            }
+            _ => FieldType::Regular,
+        },
+    }
+}
+
+fn get_macro_attribute(field: &Field) -> Option<(MacroAttribute, String)> {
+    for attr in field.attrs.iter() {
+        if !attr.path.is_ident("builder") {
+            continue;
+        }
+        let meta = attr.parse_meta().expect("builder attribute to be a meta");
+
+        let nested = match meta {
+            Meta::List(MetaList { ref nested, .. }) => nested,
+            _ => continue,
         };
 
-        setters.push(quote! {
-            fn #name(mut self, #name: #type_) -> Self {
-                self.#name = Some(#name);
-                self
-            }
-        })
-    }
+        let first_nested = nested
+            .iter()
+            .next()
+            .expect("no builder parameters provided");
 
-    let build_method = make_build_method(source);
+        let builder_attr_param = match first_nested {
+            NestedMeta::Meta(param_meta) => param_meta,
+            _ => continue,
+        };
 
-    quote! {
-        impl #builder_name {
-            #(#setters)*
-
-            #build_method
-        }
-    }
-}
-
-fn make_build_method(source: &ItemStruct) -> proc_macro2::TokenStream {
-    let mut checks = Vec::new();
-    let mut values = Vec::new();
-    
-    let source_name = &source.ident;
-
-    for field in &source.fields {
-        let name = &field.ident;
-    
-        values.push(quote! { #name: #name, });
-
-        checks.push(
-            match get_option_type(&field.ty) {
-                Ok(_) => quote!{ let #name = self.#name; },
-                Err(_) => quote!{
-                    let #name = match self.#name {
-                        Some(val) => val,
-                        None => return Err(From::from(format!("missing {} to build {}", stringify!(#name), stringify!(#source_name)))),
-                    };
-                },
-            }
-        );
-    }
-
-    quote! {
-        fn build(mut self) -> Result<#source_name, Box<dyn Error>> {
-            #(#checks)*
-            Ok(#source_name { #(#values)* })
-        }
-    }
-}
-
-fn get_option_type(type_: &Type) -> Result<Type, Box<dyn Error>> {
-    match type_ {
-        Type::Path(typepath) if typepath.qself.is_none() && path_is_option(&typepath.path) => {
-            let type_params = match &typepath.path.segments.iter().next() {
-                Some(segment) => &segment.arguments,
-                None => return Err(From::from("failed to get first segment arg"))
-            };
-
-            let parameters = match type_params {
-                syn::PathArguments::AngleBracketed(params) => params,
-                _ => return Err(From::from("no angle brackets"))
-            };
-
-            let generic_arg = match parameters.args.iter().next() {
-                Some(arg) => arg,
-                None => return Err(From::from("failed to get generic argument"))
-            };
-
-            match generic_arg {
-                syn::GenericArgument::Type(ty) => Ok(ty.clone()),
-                _ => return Err(From::from("impossible to extract type"))
-            }
-        }
-        _ => Err(From::from("not an option")),
-    }
-}
-
-fn path_is_option(path: &syn::Path) -> bool {
-    if path.leading_colon.is_none() && path.segments.len() == 1 {
-        return match path.segments.iter().next() {
-            Some(segment) => &segment.ident == "Option",
-            None => false
+        return match parse_attr_arg(&builder_attr_param) {
+            Ok((attr, value)) => Some((attr, value)),
+            Err(_) => continue,
         };
     }
-    return false;
+    None
+}
+
+fn parse_attr_arg(meta: &Meta) -> Result<(MacroAttribute, String), Box<dyn Error>> {
+    let (name, value) = match meta {
+        Meta::NameValue(MetaNameValue {
+            ref ident,
+            lit: Lit::Str(lit_str),
+            ..
+        }) => (ident.to_string(), lit_str.value()),
+        _ => return Err(From::from("not a name value attribute argument")),
+    };
+    match name.as_ref() {
+        "each" => Ok((MacroAttribute::Each, value)),
+        _ => Err(From::from(format!("unknown attribute argument {}", name))),
+    }
+}
+
+fn guess_nested_type(ty: &Type) -> Option<Type> {
+    let typepath = match ty {
+        Type::Path(typepath) if typepath.qself.is_none() => typepath,
+        _ => return None,
+    };
+    if !typepath.path.leading_colon.is_none() || typepath.path.segments.len() != 1 {
+        return None;
+    }
+    let type_params = match &typepath.path.segments.iter().next() {
+        Some(segment) => &segment.arguments,
+        None => return None,
+    };
+    let parameters = match type_params {
+        PathArguments::AngleBracketed(params) => params,
+        _ => return None,
+    };
+    let generic_arg = match parameters.args.iter().next() {
+        Some(arg) => arg,
+        None => return None,
+    };
+    match generic_arg {
+        GenericArgument::Type(ty) => Some(ty.clone()),
+        _ => return None,
+    }
+}
+
+fn get_type_name(ty: &Type) -> Option<Ident> {
+    let path = match ty {
+        Type::Path(typepath)
+            if typepath.path.leading_colon.is_none() && typepath.path.segments.len() == 1 =>
+        {
+            &typepath.path
+        }
+        _ => return None,
+    };
+    match path.segments.iter().next() {
+        Some(segment) => Some(segment.ident.clone()),
+        _ => None,
+    }
 }
