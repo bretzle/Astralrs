8630135e01613e73614242c69c066a9c24fd47cb
John Bretz <johnfish218@gmail.com>
2020-01-07 23:58:38 -0600

    builder gets created

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,10 +1,43 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse, Ident, ItemStruct};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let item: ItemStruct = parse(input).expect("failed to parse object to item struct");
+    let struct_name = &item.ident;
+    let builder_struct_name = Ident::new(&format!("{}Builder", struct_name), struct_name.span());
 
-    TokenStream::new()
+    let mut builder_struct_empty = Vec::new();
+    let mut builder_struct_fields = Vec::new();
+
+    for field in &item.fields {
+        let ident = &field.ident;
+        let ty = &field.ty;
+
+        builder_struct_fields.push(quote! {
+            #ident: Option<#ty>,
+        });
+        builder_struct_empty.push(quote! {
+            #ident: None,
+        });
+    }
+
+    let gen = quote! {
+        impl #struct_name {
+            pub fn builder() -> #builder_struct_name {
+                #builder_struct_name {
+                    #(#builder_struct_empty)*
+                }
+            }
+        }
+
+        pub struct #builder_struct_name {
+            #(#builder_struct_fields)*
+        }
+    };
+
+    gen.into()
 }
