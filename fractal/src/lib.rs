96a53916ef6b7d29481c2b093e729579a84fd928
John Bretz <johnfish218@gmail.com>
2020-01-12 11:56:32 -0600

    setup project wide lints

--- fractal/src/lib.rs
+++ fractal/src/lib.rs
@@ -1,7 +1,20 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#![deny(
+    missing_docs,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unstable_features,
+    unused_import_braces,
+    unused_qualifications,
+)]
+
+//! The Fractal Engine
+
+/// Implement this trait on your state struct so the fractal will know what to
+/// call tick on
+pub trait GameState: 'static {
+    /// Takes the current state and generate the next game state
+    fn tick(&mut self);
 }
--- rogue/src/main.rs
+++ rogue/src/main.rs
@@ -1,3 +1,19 @@
+//! A Rogue-Like game built using the Fractal Engine Crate
+
+#[macro_use]
+extern crate specs_derive;
+
+use fractal::GameState;
+use specs::prelude::*;
+
+struct State {
+    ecs: World,
+}
+
+impl GameState for State {
+    fn tick(&mut self) {}
+}
+
 fn main() {
     println!("Hello, world!");
 }
