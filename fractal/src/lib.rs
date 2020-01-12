377fcee1f80d0589381f98492e57ccddfcb715d4
John Bretz <johnfish218@gmail.com>
2020-01-12 14:41:37 -0600

    a bunch of struct declarations

--- fractal/src/lib.rs
+++ fractal/src/lib.rs
@@ -1,7 +1,5 @@
 #![deny(
     missing_docs,
-    missing_debug_implementations,
-    missing_copy_implementations,
     trivial_casts,
     trivial_numeric_casts,
     unsafe_code,
@@ -14,10 +12,23 @@
 
 
 pub mod color;
+pub mod fractal;
+pub mod font;
+pub mod console;
+pub mod graphics;
+
+pub use glutin::event::VirtualKeyCode;
+pub use graphics::shader::*;
 
 /// Implement this trait on your state struct so the fractal will know what to
 /// call tick on
 pub trait GameState: 'static {
     /// Takes the current state and generate the next game state
     fn tick(&mut self);
+}
+
+#[derive(Debug, Copy, Clone)]
+/// Wrapper for an OpenGl Platform
+pub struct Platform {
+
 }
