bd76c97d2e7e22cf7ad1907bb5a5d429af3577eb
John Bretz <bretzj@msoe.edu>
2020-01-13 15:19:29 -0600

    Work on generating a random map

--- rogue/src/main.rs
+++ rogue/src/main.rs
@@ -21,46 +21,112 @@ struct Renderable {
     fg: Color,
     bg: Color,
 }
-
-#[derive(Component)]
-struct LeftMover {}
-
-struct State {
-    ecs: World,
-}
-
+ 
 #[derive(Component, Debug)]
 struct Player {}
 
-fn try_move_player(dx: i32, dy: i32, ecs: &mut World) {
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+    Wall, Floor
+}
+
+struct State {
+    ecs: World
+}
+
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80*50];
+
+    // Make the boundaries walls
+    for x in 0..80 {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    // Now we'll randomly splat a bunch of walls. It won't be pretty, but it's a decent illustration.
+    // First, obtain the thread-local RNG:
+    let mut rng = fractal::RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
+}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
 
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79 , max(0, pos.x + dx));
-        pos.y = min(49, max(0, pos.y + dy));
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(79 , max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
     }
 }
 
 fn player_input(gs: &mut State, ctx: &mut Fractal) {
+    // Player movement
     match ctx.key {
+        None => {} // Nothing happened
         Some(key) => match key {
             VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
             VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
             VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
             VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
             _ => {}
+        },
+    }
+}
+
+fn draw_map(map: &[TileType], ctx : &mut Fractal) {
+    let mut y = 0;
+    let mut x = 0;
+    for tile in map.iter() {
+        // Render a tile depending upon the tile type
+        match tile {
+            TileType::Floor => {
+                ctx.set(x, y, (127, 127, 127).into(), (0, 0, 0).into(), fractal::to_keycode('.'));
+            }
+            TileType::Wall => {
+                ctx.set(x, y, (0, 255, 0).into(), (0, 0, 0).into(), fractal::to_keycode('#'));
+            }
         }
-        None => {}
+
+        // Move the coordinates
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
     }
 }
 
 impl GameState for State {
-    fn tick(&mut self, ctx: &mut Fractal) {
+    fn tick(&mut self, ctx : &mut Fractal) {
         ctx.cls();
 
         player_input(self, ctx);
         self.run_systems();
+
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -71,36 +137,22 @@ impl GameState for State {
     }
 }
 
-struct LeftWalker {}
-
-impl<'a> System<'a> for LeftWalker {
-    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
-
-    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
-        for (_lefty, pos) in (&lefty, &mut pos).join() {
-            pos.x -= 1;
-            if pos.x < 0 {
-                pos.x = 79;
-            }
-        }
-    }
-}
-
 impl State {
     fn run_systems(&mut self) {
-        let mut lw = LeftWalker {};
-        lw.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
 
 fn main() {
     let context = Fractal::init_simple(80, 50, "Hello Rust World", "resources");
-    let mut gs = State { ecs: World::new() };
+    let mut gs = State {
+        ecs: World::new()
+    };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-    gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
+
+    gs.ecs.insert(new_map());
 
     gs.ecs
         .create_entity()
@@ -112,19 +164,6 @@ fn main() {
         })
         .with(Player{})
         .build();
-
-    for i in 0..10 {
-        gs.ecs
-            .create_entity()
-            .with(Position { x: i * 7, y: 20 })
-            .with(Renderable {
-                glyph: fractal::to_keycode('☺'),
-                fg: color::RED,
-                bg: color::BLACK,
-            })
-            .with(LeftMover {})
-            .build();
-    }
 
     fractal::main_loop(context, gs);
 }
