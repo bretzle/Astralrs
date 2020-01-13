06994cee602c49ff1a270362396e637c0a007742
John Bretz <johnfish218@gmail.com>
2020-01-13 13:46:18 -0600

    Started work on getting animation possible

--- fractal/src/console.rs
+++ fractal/src/console.rs
@@ -17,27 +17,107 @@ pub struct Tile {
 
 /// Trait that must be implemented by console types
 pub trait Console {
-    /// Clears the screen.
-    fn cls(&mut self);
+    /// Check to see if the internal OpenGL representation needs to be rebuilt, and do so if required.
+    fn rebuild_if_dirty(&mut self, platform: &hal::RltkPlatform);
 
-    /// Sets a single cell in the console.
-    fn set(&mut self, x: i32, y: i32, fg: Color, bg: Color, glyph: u8);
+    /// Gets the dimensions of the console in characters
+    fn get_char_size(&mut self) -> (u32, u32);
 
-    /// Translate an (x, y) coordinate into an array index.
-    fn at(&self, x: i32, y: i32) -> usize {
-        unimplemented!();
-    }
-
-    /// Tells the console to draw itself to the screen.
-    fn draw(&mut self, font: &Font, shader: &Shader, platform: &Platform);
-
-    /// Resizes pixels
+    // Resizes the viewport
     fn resize_pixels(&mut self, width: u32, height: u32);
 
-    /// Check to see if the internal OpenGL representation needs to be rebuilt, and do so if required.
-    fn rebuild_if_dirty(&mut self, platform: &Platform) {
-        unimplemented!();
-    }
+    /// Tells the console to draw itself via OpenGL.
+    fn gl_draw(&mut self, font: &Font, shader: &Shader, platform: &hal::RltkPlatform);
+
+    /// Converts an x/y coordinate to a console index number.
+    fn at(&self, x: i32, y: i32) -> usize;
+
+    /// Clear the console.
+    fn cls(&mut self);
+
+    /// Clear the console to a set background color, if supported.
+    fn cls_bg(&mut self, background: Color);
+
+    /// Print a string at the specified x/y coordinate.
+    fn print(&mut self, x: i32, y: i32, output: &str);
+
+    /// Print a string in color at the specified x/y coordinate, with specified foreground and background.
+    fn print_color(&mut self, x: i32, y: i32, fg: Color, bg: Color, output: &str);
+
+    /// Sets a single cell to a color/glyph combination.
+    fn set(&mut self, x: i32, y: i32, fg: Color, bg: Color, glyph: u8);
+
+    /// Sets a single cell's background color.
+    fn set_bg(&mut self, x: i32, y: i32, bg: Color);
+
+    /// Draws a box, starting at x/y with the extents width/height using CP437 line characters
+    fn draw_box(&mut self, x: i32, y: i32, width: i32, height: i32, fg: Color, bg: Color);
+
+    /// Draws a box, starting at x/y with the extents width/height using CP437 line characters,
+    /// without filling in the middle
+    fn draw_hollow_box(&mut self, x: i32, y: i32, width: i32, height: i32, fg: Color, bg: Color);
+
+    /// Draws a box, starting at x/y with the extents width/height using CP437 double line characters
+    fn draw_box_double(&mut self, x: i32, y: i32, width: i32, height: i32, fg: Color, bg: Color);
+
+    /// Draws a box, starting at x/y with the extents width/height using CP437 double line characters,
+    /// without filling in the middle
+    fn draw_hollow_box_double(
+        &mut self,
+        x: i32,
+        y: i32,
+        width: i32,
+        height: i32,
+        fg: Color,
+        bg: Color,
+    );
+
+    /// Fills a rectangle-defined region with a given glyph
+    // fn fill_region(&mut self, target: Rect, glyph: u8, fg: Color, bg: Color);
+
+    /// Retrieve a given cell in the console, if present
+    fn get(&self, x: i32, y: i32) -> Option<(&u8, &Color, &Color)>;
+
+    /// Draws a horizontal progress bar.
+    fn draw_bar_horizontal(
+        &mut self,
+        x: i32,
+        y: i32,
+        width: i32,
+        n: i32,
+        max: i32,
+        fg: Color,
+        bg: Color,
+    );
+
+    /// Draws a vertical progress bar.
+    fn draw_bar_vertical(
+        &mut self,
+        x: i32,
+        y: i32,
+        height: i32,
+        n: i32,
+        max: i32,
+        fg: Color,
+        bg: Color,
+    );
+
+    /// Prints text, centered to the whole console width, at vertical location y.
+    fn print_centered(&mut self, y: i32, text: &str);
+
+    /// Prints text in color, centered to the whole console width, at vertical location y.
+    fn print_color_centered(&mut self, y: i32, fg: Color, bg: Color, text: &str);
+
+    /// Serializes the console layer to an XpFile
+    // fn to_xp_layer(&self) -> XpLayer;
+
+    /// Specify a global offset (by character count, so 0.5 is half a character). Useful for
+    /// drawing walls between tiles.
+    fn set_offset(&mut self, x: f32, y: f32);
+
+    // Produces the implementor as an Any that can be matched to determine type and access
+    // natively.
+    // fn as_any(&self) -> &dyn Any;
 }
 
 pub fn log<S: ToString>(message: S) {
--- rogue/src/main.rs
+++ rogue/src/main.rs
@@ -20,6 +20,8 @@ struct Renderable {
     bg: Color,
 }
 
+#[derive(Component)]
+struct LeftMover {}
 struct State {
     ecs: World,
 }
@@ -27,6 +29,9 @@ struct State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Fractal) {
         ctx.cls();
+
+        self.run_systems();
+
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
 
@@ -36,11 +41,35 @@ impl GameState for State {
     }
 }
 
+struct LeftWalker {}
+
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+
+    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
+        for (_lefty, pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 {
+                pos.x = 79;
+            }
+        }
+    }
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftWalker {};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
+}
+
 fn main() {
     let context = Fractal::init_simple(80, 50, "Hello Rust World", "resources");
     let mut gs = State { ecs: World::new() };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
 
     gs.ecs
         .create_entity()
@@ -61,6 +90,7 @@ fn main() {
                 fg: color::RED,
                 bg: color::BLACK,
             })
+            .with(LeftMover {})
             .build();
     }
 
