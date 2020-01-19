a9743ac391c1f3a82849ef72739bf7f9c53fde1b
John Bretz <johnfish218@gmail.com>
2020-01-19 09:14:07 -0600

    Multiple monsters can spawn per room

--- rogue/src/map.rs
+++ rogue/src/map.rs
@@ -11,9 +11,9 @@ use fractal::random::RandomNumberGenerator;
 use specs::prelude::*;
 use std::cmp::{max, min};
 
-const MAPWIDTH: usize = 80;
-const MAPHEIGHT: usize = 50;
-const MAPCOUNT: usize = MAPHEIGHT * MAPWIDTH;
+pub const MAPWIDTH: usize = 80;
+pub const MAPHEIGHT: usize = 50;
+pub const MAPCOUNT: usize = MAPHEIGHT * MAPWIDTH;
 
 #[derive(PartialEq, Copy, Clone)]
 pub enum TileType {
