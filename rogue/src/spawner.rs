f5fadbb3ffc73b9a7137715060e9baaa3ec6f450
John Bretz <johnfish218@gmail.com>
2020-01-20 15:41:36 -0600

    Fixed player health being high

--- rogue/src/spawner.rs
+++ rogue/src/spawner.rs
@@ -32,8 +32,8 @@ pub fn player(ecs: &mut World, player_x: i32, player_y: i32) -> Entity {
             name: "Player".to_string(),
         })
         .with(CombatStats {
-            max_hp: 300,
-            hp: 300,
+            max_hp: 30,
+            hp: 30,
             defense: 2,
             power: 5,
         })
